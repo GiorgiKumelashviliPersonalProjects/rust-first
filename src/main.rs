c2a100f3555c2a6993c43a1bef340be8b69740df
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-08-28 16:38:05 +0400

    Testing rust

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![allow(non_snake_case)]
+
+fn main() {
+    // A tuple with a bunch of different types
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    println!("this is {:?}", long_tuple);
+}

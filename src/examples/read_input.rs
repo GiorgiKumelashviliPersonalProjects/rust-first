abadc7275c91253f0a6e91f412a7fb629df82196
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2023-01-22 23:44:58 +0400

    guessing game implmemented

--- src/examples/read_input.rs
+++ src/examples/read_input.rs
@@ -1,10 +1,9 @@
 #![allow(non_snake_case)]
 #![allow(unused_assignments)]
 
-// use rand::Rng;
-// use std::io;
-
+use rand::Rng;
 use std::io::{self, Write};
+use std::{cmp::Ordering, io::Write};
 
 fn read_input() {
     let mut input: String = String::new();
@@ -14,4 +13,37 @@ fn read_input() {
     println!("{}", input);
 }
 
-fn main() {}
+fn guessing_game() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut try_count = 0;
+
+    println!("{secret_number}");
+
+    loop {
+        print!("Please input your guess: ");
+
+        let mut guess = String::new();
+
+        std::io::stdout().flush().unwrap();
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You guessed: {guess}, Try count: {try_count}");
+                break;
+            }
+            Ordering::Less => println!("More"),
+            Ordering::Greater => println!("Low"),
+        }
+
+        try_count += 1;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,38 @@ mod another;
 mod file_read;
 mod test;
 
-use crate::another::print_hello;
-use file_read::read_write_file;
+use rand::Rng;
+use std::{cmp::Ordering, io::Write};
 
 pub fn print_lines(param: Option<&str>) {
     println!("{}", "=".repeat(40) + " " + param.unwrap_or(""));
 }
 
+fn get_num() -> i32 {
+    return 5;
+}
+
 fn main() {
-    print_hello();
-    let basic_closure = |age: i32| {
-        return age > 18;
-    };
-
     print_lines(None);
-    test::some_test_module::another_one::say_hello();
-    read_write_file();
+    // let x = 5;
+    // let y = 10;
 
-    let arr = [1, 2, 3, 4];
+    // println!("x = {x} and y + 2 = {}", y + 2);
+    // println!("{:?}", get_num());
 
-    for val in arr.iter() {
-        println!("{}", val);
-    }
+    // print_hello();
+
+    // let _basic_closure = |age: i32| {
+    //     return age > 18;
+    // };
+
+    // print_lines(None);
+    // test::some_test_module::another_one::say_hello();
+    // read_write_file();
+
+    // let arr = [1, 2, 3, 4];
+
+    // for val in arr.iter() {
+    //     println!("{}", val);
+    // }
 }
